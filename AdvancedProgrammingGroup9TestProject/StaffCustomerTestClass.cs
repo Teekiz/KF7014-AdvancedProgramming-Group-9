da339fd6bad92aa820078288f825b1fa462bbffa
Callum Rossiter <callumdavidrossiter@gmail.com>
2021-12-10 12:25:13 +0000

    Staff and customer test template =.

---
+++ AdvancedProgrammingGroup9TestProject/StaffCustomerTestClass.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using AdvancedProgrammingGroup9;
+
+namespace AdvancedProgrammingGroup9TestProject
+{
+    [TestClass]
+    class StaffCustomerTestClass
+    {
+        Customer customer;
+        Staff staff;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            //customer
+            //staff
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            customer = null;
+            staff = null;
+        }
+
+        [TestMethod]
+        public void StaffTesting()
+        {
+            //
+        }
+
+        [TestMethod]
+        public void CustomerTesting()
+        {
+            //
+        }
+    }
+}
