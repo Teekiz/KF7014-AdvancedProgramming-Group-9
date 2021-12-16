c89ed15e4650680a7728ef33c83147dce032c55e
Saipavan188 <95652387+Saipavan188@users.noreply.github.com>
2021-12-16 09:51:30 +0000

    Update StaffCustomerTestClass.cs

--- AdvancedProgrammingGroup9TestProject/StaffCustomerTestClass.cs
+++ AdvancedProgrammingGroup9TestProject/StaffCustomerTestClass.cs
@@ -16,6 +16,12 @@ namespace AdvancedProgrammingGroup9TestProject
         {
             //customer
             //staff
+            Staff = new StaffModel(StaffId.Now, Firstname.Now,Lastname.Now,Workinghours.now,Adressid.now,AddID(1));
+            StaffID1 = new staffIDModel("SAI", 2, null);
+            Create = new DatabaseCreateQueries();
+
+            
+            
         }
 
         [TestCleanup]
@@ -28,7 +34,16 @@ namespace AdvancedProgrammingGroup9TestProject
         [TestMethod]
         public void StaffTesting()
         {
-            //
+            //staff
+            
+             StaffId now = StaffID.Now;
+            StaffId1 = new StaffIDModel(now, StaffId.Now.AddId(1));
+            Assert.AreEqual(now, Firstname.GetReceivedFirstname(), "Received Firstname Incorrect");
+            Assert.AreEqual(now, Lastname.GetReceivedlastname(), "Received Lastname Incorrect");
+            Assert.AreEqual(now, Workinghours.GetReceivedworkinghours(), "Received Workinghours Incorrect");
+            Assert.AreEqual(now, AddressId.GetReceivedAddressId(), "Received AddressId Incorrect");
+            
+            
         }
 
         [TestMethod]
