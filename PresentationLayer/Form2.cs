06ea39e63e3d7b3209afaea4f45910fa4ac8d338
Lewis Barton <w18008993@northumbria.ac.uk>
2021-12-28 14:36:08 +0000

    interface for form 2

--- PresentationLayer/Form2.cs
+++ PresentationLayer/Form2.cs
@@ -10,6 +10,32 @@ using System.Windows.Forms;
 
 namespace PresentationLayer
 {
+    public interface IOrderManager
+    {
+        DateTime requestedTTC { set; }
+        DateTime estimatedTTC { set; }
+        DateTime currentTTC { set; }
+        DateTime orderDate { set; }
+        DateTime custBirthDate { set; }
+        string priceOffer { get; set; }
+        string manOrderNotes { get; set; }
+        string custCounty { set; }
+        string custCountry { set; }
+        string custTownCity { set; }
+        string custAddressline1 { set; }
+        string custAddressline2 { set; }
+        string custPhone { set; }
+        string custPostcode { set; }
+        string custOrderNotes { set; }
+        string systemRec { set; }
+        string acceptOrderRadioButton();
+        string customerTypeRadioButton();
+
+        void Form2_Load(object sender, EventArgs e);
+        void label4_Click(object sender, EventArgs e);
+        void label18_Click(object sender, EventArgs e);
+    }
+
     public partial class OrderManager : Form
     {
         public OrderManager()
