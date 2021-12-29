bca9f0120cd7b9833bd27613b762744d1d4f87cb
Saipavan188 <95652387+Saipavan188@users.noreply.github.com>
2021-12-29 23:46:41 +0000

    Update Schedule.cs

--- PresentationLayer/Schedule.cs
+++ PresentationLayer/Schedule.cs
@@ -98,6 +98,20 @@ namespace PresentationLayer
                     order.confirmedDeadline = screen.deadline;
                     Schedule.SaveOrder(order, Schedule);
                 }
+                
+                public IEnumerable<DateTime> GenerateDates(DateTime start, DateTime end)
+                   {
+                 var dates = new List<DateTime>();
+
+                  var date = new DateTime(start.Year, start.Month, start.Day);
+                while (date.Month <= end.Month && date.Year <= end.Year)
+                 {
+           date = date.Add1Months(1);
+               dates.Add(date);
+                 }
+
+              return dates;
+                 } 
             }
         }
     }
