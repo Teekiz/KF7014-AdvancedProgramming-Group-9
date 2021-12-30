c744e592be12df883d603982ce72f271bd160527
Lewis Barton <w18008993@northumbria.ac.uk>
2021-12-30 04:59:34 +0000

    Fixed Form 1 images.

--- PresentationLayer/Properties/Resources.Designer.cs
+++ PresentationLayer/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace PresentationLayer.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap estimates {
+            get {
+                object obj = ResourceManager.GetObject("estimates", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap imageonline_co_roundcorner {
             get {
                 object obj = ResourceManager.GetObject("imageonline-co-roundcorner", resourceCulture);
