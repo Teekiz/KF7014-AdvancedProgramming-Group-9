6f72df3d1c67f1ae374f07447216f5a429c9ab41
Callum <callumdavidrossiter@gmail.com>
2021-12-31 16:53:41 +0000

    Stronger Encapsulation.

--- DataAccessLayer/DataAccessEntities.cs
+++ DataAccessLayer/DataAccessEntities.cs
@@ -20,43 +20,59 @@ namespace DataAccessLayer
     {
         public Enquiry()
         {
-            orderItemsList = new List<OrderItems>();
+            OrderItemsList = new List<OrderItems>();
         }
 
-        [Key]
-        public int orderID { get; set; }
-        public DateTime receivedDate { get; set; }
-        public DateTime deadline { get; set; }
-        public double price { get; set; }
-        public int hoursToComplete { get; set; }
-        public string orderStatus { get; set; }
-        public string orderNotes { get; set; }
-        //[Required, ForeignKey("orderID")]
-        //[Required]
+        private int OrderID;
+        private DateTime ReceivedDate;
+        private DateTime Deadline;
+        private double Price;
+        private int HoursToComplete;
+        private string OrderStatus;
+        private string OrderNotes;
+        private Customer customer; //careful, this is inverted virtual
+        private ICollection<OrderItems> OrderItemsList; //virtual
 
-        public virtual Customer Customer { get; set; }
-        public virtual ICollection<OrderItems> orderItemsList { get; set; }
+        [Key]
+        public int orderID { get { return OrderID; } set { OrderID = value; } }
+        public DateTime receivedDate { get { return ReceivedDate; } set { ReceivedDate = value; } }
+        public DateTime deadline { get { return Deadline; } set { Deadline = value; } }
+        public double price { get { return Price; } set { Price = value; } }
+        public int hoursToComplete { get { return HoursToComplete; } set { HoursToComplete = value; } }
+        public string orderStatus { get { return OrderStatus; } set { OrderStatus = value; } }
+        public string orderNotes { get { return OrderNotes; } set { OrderNotes = value; } }
+        public virtual Customer Customer { get { return customer; } set { customer = value; } }
+        public virtual ICollection<OrderItems> orderItemsList { get { return OrderItemsList; } set { OrderItemsList = value; } }
     }
 
     public class OrderItems
     {
         public OrderItems() {}
 
+        private int ItemID;
+        private string Description;
+        private int Quantity;
+        private byte[] ReferenceImage; //referenceImage is optional for the user - this code was modified based on stack overflow (#1).
+        private int MaxTime;
+        private int MinTime;
+        private double MinCost;
+        private double MaxCost;
+        private Enquiry enquiry;
+
         [Key]
-        public int itemID { get; set; }
-        public string description { get; set; }
-        public int quantity { get; set; }
-        //referenceImage is optional for the user - this code was modified based on stack overflow (#1).
-        public byte[] referenceImage { get; set; }
+        public int itemID { get { return ItemID; } set { ItemID = value; } }
+        public string description { get { return Description; } set { Description = value; } }
+        public int quantity { get { return Quantity; } set { Quantity = value; } }
+        public byte[] referenceImage { get { return ReferenceImage; } set { ReferenceImage = value; } }
         [NotMapped]
-        public int maxTime { get; set; }
+        public int maxTime { get { return MaxTime; } set { MaxTime = value; } }
         [NotMapped]
-        public int minTime { get; set; }
+        public int minTime { get { return MinTime; } set { MinTime = value; } }
         [NotMapped]
-        public double minCost { get; set; }
+        public double minCost { get { return MinCost; } set { MinCost = value; } }
         [NotMapped]
-        public double maxCost { get; set; }
-        public virtual Enquiry Enquiry { get; set; }
+        public double maxCost { get { return MaxCost; } set { MaxCost = value; } }
+        public virtual Enquiry Enquiry { get { return enquiry; } set { enquiry = value; } }
 
         public void getItemTime(out int minCalcTime, out int maxCalcTime)
         {
@@ -108,42 +124,66 @@ namespace DataAccessLayer
     public class Order
     {
         public Order() { }
+
+        private int OrderID;
+        private DateTime ScheduledStartDate;
+        private DateTime ConfirmedDeadline;
+        private int ProgressCompleted;
+        private Enquiry enquiry;
+
         [Key]
-        public int orderID { get; set; }
-        public DateTime scheduledStartDate { get; set; }
-        public DateTime confirmedDeadline { get; set; }
-        public int progressCompleted { get; set; }
-        public virtual Enquiry Enquiry { get; set; }
+        public int orderID { get { return OrderID; } set { OrderID = value; } }
+        public DateTime scheduledStartDate { get { return ScheduledStartDate; } set { ScheduledStartDate = value; } }
+        public DateTime confirmedDeadline { get { return ConfirmedDeadline; } set { ConfirmedDeadline = value; } }
+        public int progressCompleted { get { return ProgressCompleted; } set { ProgressCompleted = value; } }
+        public virtual Enquiry Enquiry { get { return enquiry; } set { enquiry = value; } }
     }
 
     public class Customer
     {
         public Customer() { }
 
+        //these were previosuly public
+        private int CustomerID;
+        private string Name;
+        private string Addressline1;
+        private string Phone;
+        private string Addressline2;
+        private DateTime Birthdate;
+        private string Postcode;
+        private string TownCity;
+        private string County;
+        private string Country;
+        private string Type;
+
         [Key]
-        public int customerID { get; set; }
-        public string name { get; set; }
-        public string addressline1 { get; set; }
-        public string phone { get; set; }
-        public string addressline2 { get; set; }
-        public DateTime birthdate { get; set; }
-        public string postcode { get; set; }
-        public string townCity { get; set; }
-        public string county { get; set; }
-        public string country { get; set; }
-        public string type { get; set; }
-        //public virtual Enquiry Enquiry { get; set; }
+        public int customerID { get { return CustomerID; } set { CustomerID = value; } }
+        public string name { get { return Name; } set { Name = value; } }
+        public string addressline1 { get { return Addressline1; } set { Addressline1 = value; } }
+        public string addressline2 { get { return Addressline2; } set { Addressline2 = value; } }
+        public string phone { get { return Phone; } set { Phone = value; } }
+        public DateTime birthdate { get { return Birthdate; } set { Birthdate = value; } }
+        public string postcode { get { return Postcode; } set { Postcode = value; } }
+        public string townCity { get { return TownCity; } set { TownCity = value; } }
+        public string county { get { return County; } set { County = value; } }
+        public string country { get { return Country; } set { Country = value; } }
+        public string type { get { return Type; } set { Type = value; } }
     }
     public class Staff
     {
         public Staff() {}
 
+        private int StaffId;
+        private string Firstname;
+        private string Lastname;
+        private string Username;
+        private string Password;
+
         [Key]
-        public int staffId { get; set; }
-        public string firstname { get; set; }
-        public string lastname { get; set; }
-        public string username { get; set; }
-        public string password { get; set; }
-        //public virtual Enquiry Enquiry {get;set}
+        public int staffId { get { return StaffId; } set { StaffId = value; } }
+        public string firstname { get { return Firstname; } set { Firstname = value; } }
+        public string lastname { get { return Lastname; } set { Lastname = value; } }
+        public string username { get { return Username; } set { Username = value; } }
+        public string password { get { return Password; } set { Password = value; } }
     }
 }
