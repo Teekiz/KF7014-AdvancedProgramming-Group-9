6569207e3faf8db8ee8acf8d70c0b3f8bcbf3032
Lewis Barton <w18008993@northumbria.ac.uk>
2022-01-01 03:00:40 +0000

    Changed font on Credits page

--- PresentationLayer/Credits.Designer.cs
+++ PresentationLayer/Credits.Designer.cs
@@ -37,11 +37,11 @@ namespace PresentationLayer
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(244, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(288, 29);
+            this.label1.Size = new System.Drawing.Size(302, 29);
             this.label1.TabIndex = 1;
             this.label1.Text = "Credits and References";
             // 
